2783ea463851522d6cbf20aac3d8ee02a787c9ff
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-07-29 20:04:13 +0800

    Update AdminHammer.cs

--- AdminHammer.cs
+++ AdminHammer.cs
@@ -1,7 +1,8 @@
-||| Permissions
+<perm>
+1. |adminhammer.allow|   !Allows player to use the admin hammer!
+</perm>
 
-|adminhammer.allow| -- !Allows player to use the admin hammer!
-\\\ Commands
 
-\/adminhammer\ !Toggle AdminHammer mode!
-\/b\ !Check entity command!
+<comm>
+1. |/adminhammer| !Toggle AdminHammer mode!
+</comm>
