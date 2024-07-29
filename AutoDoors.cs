e34142926489baf4332157f513814c745da8cbed
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-07-29 23:29:09 +0800

    Create AutoDoors.cs

---
+++ AutoDoors.cs
@@ -0,0 +1,14 @@
+<perm>
+1. |  autodoors.use   | <t>    Allows player to toggle and have automatic doors   </t>
+</perm>
+
+<comm>
+1. |  /ad   | <t>   Enable/Disable automatic door closing    </t>
+2. |  /ad <time (seconds)>   | <t>   Set automatic closing delay for doors (Doors set by 'single' and 'type' are not included).    </t>
+3. |  /ad <all | a> <time (seconds)>   | <t>    Set automatic closing delay for all doors.   </t>
+4. |   /ad <single | s>  | <t>    Enable/Disable automatic closing of the door you are looking at   </t>
+5. |  /ad <single | s> <time (seconds)>   | <t>   Set automatic closing delay for the door you are looking at    </t>
+6. |   /ad <type | t>  | <t>    Enable/disable automatic door closing for the type of door you are looking at   </t>
+7. |  /ad <type | t> <time (seconds)>   | <t>   Set automatic closing delay for the type of door you are looking at    </t>
+8. |  /ad <help | h>   | <t>   View help    </t>
+</comm>
