24a34d2caede201f0dbc85e3159bd31119c1efa2
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-07-29 23:17:12 +0800

    Create AdminPanel.cs

---
+++ AdminPanel.cs
@@ -0,0 +1,19 @@
+<perm>
+1. |   adminpanel.autotoggle.admin    |   <t>Toggles panel when added/removed from the admin group</t>
+2. |   adminpanel.allowed    |   <t>Allows player the ability to see the admin panel</t>
+3. |   adminradar.allowed    |   <t>-</t>
+4. |    godmode.toggle   |   <t>-</t>
+5. |  vanish.use     |   <t>-</t>
+6. |  playeradministration.access.show   | <t>-</t>
+</perm>
+
+
+<comm>
+
+1. |  /adminpanel hide   | <t>Hide the admin panel</t>
+2. |  /adminpanel show  | <t>Show/refresh the admin panel</t>
+3. |  /adminpanel settp   | <t>Sets your custom teleportation location (on player's location)</t>
+4. |  /adminpanel settp all   | <t>Sets teleport location for all admins without a custom location</t>
+5. |  /adminpanel removetp   | <t>Removes your custom teleport location</t>
+
+</comm>
