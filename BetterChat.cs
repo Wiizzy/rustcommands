da88c48e4c7685788e5e0d85ed0d9022831f3a10
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-07-29 23:34:21 +0800

    Create BetterChat.cs

---
+++ BetterChat.cs
@@ -0,0 +1,12 @@
+<perm>
+1. |  betterchat.admin    |   <t>Required to use the the chat command</t>
+</perm>
+
+<comm>
+1. |  chat group add <group>   | <t>    Creates a new chat group   </t>
+2. |   chat group remove <group>  | <t>    Removes a chat group   </t>
+3. |  chat group set <group> <setting> <value>   | <t>    Changes a chat group setting   </t>
+4. |  chat group list   | <t>   Lists all chat groups    </t>
+5. |  chat user add <player|steamid> <group>   | <t>   Adds a player to a chat group    </t>
+6. |   chat user remove <player|steamid> <group>  | <t>     Removes a player from a chat group  </t>
+</comm>
