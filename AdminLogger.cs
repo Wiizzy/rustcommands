36a7c69797c494ae5689c6247a19b59df83c7225
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-07-29 23:12:46 +0800

    Create AdminLogger.cs

---
+++ AdminLogger.cs
@@ -0,0 +1,22 @@
+<perm>
+
+
+
+
+1. |   NONE    |   <t>Admin Logger logs the usage of almost all default admin commands and stores it in a file. It can also log admin plugins usage (Vanish, AdminRadar etc.). Useful for server owners that want to keep an eye on their admins. Fully customizable through config file. Logs are stored under the oxide/logs/AdminLogger folder.</t>
+
+
+
+</perm>
+
+
+<comm>
+
+
+
+1. |  NONE   | <t>The default messages are in the AdminLogger file under the lang/en directory. To add support for another language, create a new language folder (e.g. de for German) if not already created, copy the default language file to the new folder and then customize the messages.</t>
+
+
+
+
+</comm>
