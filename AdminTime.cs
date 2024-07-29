5f0faeb6759c658744b4d033b2e4aa7cfbd27c4c
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-07-29 23:23:13 +0800

    Create AdminTime.cs

---
+++ AdminTime.cs
@@ -0,0 +1,10 @@
+<comm>
+1. |  /day   | <t>   Changes the players local time to configured day value default 12    </t>
+2. |  /night   | <t>   Changes the players local time to configured night value default 1    </t>
+3. |  /time <1-23>  | <t>  Changes to the specified time (1-23)     </t>
+4. |  /now   | <t>   Reverts all local time changes to the global time of the server.    </t>
+</comm>
+
+<perm>
+1. |  AdminTime.use   | <t>   Allows users to change client time without use of moderatorid or ownerid.    </t>
+</perm>
