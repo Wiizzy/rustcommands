af1d8a6d653422cc925308a4f83fed188e0250bb
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-03 15:19:28 +0800

    Create FurnaceSplitter.cs

---
+++ FurnaceSplitter.cs
@@ -0,0 +1,10 @@
+<perm>
+1. |furnacesplitter.use|   <t>Allows player to use the furnace splitting functionality</t>
+</perm>
+
+
+<comm>
+1. |/fs| <t>Shows the status and help options</t>
+2. |/fs on| <t>o enable Furnace Splitter</t>
+3. |/fs off| <t>To disable Furnace Splitter</t>
+</comm>
