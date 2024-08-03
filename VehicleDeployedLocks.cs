e88e6adffbb8c3500221ebdc6fad0a291b687bef
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-03 20:39:32 +0800

    Create VehicleDeployedLocks.cs

---
+++ VehicleDeployedLocks.cs
@@ -0,0 +1,63 @@
+<perm>
+1. |vehicledeployedlocks.masterkey| -- <t>Allows the player to open any lock managed by this plugin, useful for allowing admins access to locked vehicles.</t>
+2. |vehicledeployedlocks.codelock.free| -- <t>Allows the player to deploy code locks to vehicles without consuming locks or resources from their inventory.</t>
+</perm>
+
+<comm>
+1. |vehicledeployedlocks.masterkey| -- <t>Allows the player to open any lock managed by this plugin, useful for allowing admins access to locked vehicles.</t>
+2. |vehicledeployedlocks.codelock.allvehicles| <t>(all in one)</t>
+3. |vehicledeployedlocks.codelock.attackhelicopter|
+4. |vehicledeployedlocks.codelock.chinook|
+5. |vehicledeployedlocks.codelock.duosub|
+6. |vehicledeployedlocks.codelock.hotairballoon|
+7. |vehicledeployedlocks.codelock.kayak|
+8. |vehicledeployedlocks.codelock.locomotive|
+9. |vehicledeployedlocks.codelock.magnetcrane|
+10. |vehicledeployedlocks.codelock.minicopter|
+11. |vehicledeployedlocks.codelock.modularcar|
+12. |vehicledeployedlocks.codelock.motorbike.sidecar|
+13. |vehicledeployedlocks.codelock.motorbike|
+14. |vehicledeployedlocks.codelock.pedalbike|
+15. |vehicledeployedlocks.codelock.pedaltrike|
+16. |vehicledeployedlocks.codelock.rowboat|
+17. |vehicledeployedlocks.codelock.rhib|
+18. |vehicledeployedlocks.codelock.ridablehorse|
+19. |vehicledeployedlocks.codelock.scraptransport|
+20. |vehicledeployedlocks.codelock.sedan|
+21. |vehicledeployedlocks.codelock.sedanrail|
+22. |vehicledeployedlocks.codelock.snowmobile|
+23. |vehicledeployedlocks.codelock.solosub|
+24. |vehicledeployedlocks.codelock.tomaha|
+25. |vehicledeployedlocks.codelock.tugboat|
+26. |vehicledeployedlocks.codelock.workcart|
+27. |vehicledeployedlocks.codelock.workcartaboveground|
+28. |vehicledeployedlocks.codelock.workcartcovered|
+29. |vehicledeployedlocks.keylock.free| -- <t>Allows the player to deploy key locks to vehicles without consuming locks or resources from their inventory.|</t>
+30. |vehicledeployedlocks.keylock.allvehicles|  -- <t>(all in one) The following permissions allow players to deploy key locks to vehicles.</t>
+31. |vehicledeployedlocks.keylock.attackhelicopter|
+32. |vehicledeployedlocks.keylock.chinook|
+33. |vehicledeployedlocks.keylock.duosub|
+34. |vehicledeployedlocks.keylock.hotairballoon|
+35. |vehicledeployedlocks.keylock.kayak|
+36. |vehicledeployedlocks.keylock.locomotive|
+37. |vehicledeployedlocks.keylock.magnetcrane|
+38. |vehicledeployedlocks.keylock.minicopter|
+39. |vehicledeployedlocks.keylock.modularcar|
+40. |vehicledeployedlocks.keylock.motorbike.sidecar|
+41. |vehicledeployedlocks.keylock.motorbike|
+42. |vehicledeployedlocks.keylock.pedalbike|
+43. |vehicledeployedlocks.keylock.pedaltrike|
+44. |vehicledeployedlocks.keylock.rhib|
+45. |vehicledeployedlocks.keylock.ridablehorse|
+46. |vehicledeployedlocks.keylock.rowboat|
+47. |vehicledeployedlocks.keylock.scraptransport|
+48. |vehicledeployedlocks.keylock.sedan|
+49. |vehicledeployedlocks.keylock.sedanrail|
+50. |vehicledeployedlocks.keylock.snowmobile|
+51. |vehicledeployedlocks.keylock.solosub|
+52. |vehicledeployedlocks.keylock.tomaha|
+53. |vehicledeployedlocks.keylock.tugboat|
+54. |vehicledeployedlocks.keylock.workcart|
+55. |vehicledeployedlocks.keylock.workcartaboveground|
+56. |vehicledeployedlocks.keylock.workcartcovered|
+</comm>
