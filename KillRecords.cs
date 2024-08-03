907aa0ce4a74e0340295fd56843040ee1978a3d5
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-03 19:39:33 +0800

    Create KillRecords.cs

---
+++ KillRecords.cs
@@ -0,0 +1,31 @@
+<perm>
+1. |killrecords.admin| -- <t>Gives permission to use all admin chat commands</t>
+2. |killrecords.killchat| -- <t>Allows player to turn on/off kill messages</t>
+</perm>
+
+<comm>
+1. |/krhelpadmin| - <t>Shows all admin commands in chat</t>
+2. |/killsweb| - <t>Sends data over webrequest</t>
+3. |/krbackup| - <t>saved datafile when using SQL</t>
+4. |/krsql update| - <t>Updates your records in SQL</t>
+5. |/krsql check| - <t>Checks SQL to see if your records exist, if not will create if SQL enabled</t>
+6. |/krsql checkall| - <t>Checks SQL to see if all records exist from cache, if not will create if SQL enabled</t>
+7. |/resetkillrecords| - <t>Clears and resets all player data in Kill Records</t>
+8. |/krhelp| - <t>Shows all commands in chat</t>
+9. |/pkills| - <t>Shows your kill records UI</t>
+10. |/pkills (playername)| - <t>Shows another players kill records UI</t>
+11. |/pkillschat| - <t>Shows your kill records in chat</t>
+12. |/pkillschat (playername)| - <t>Shows another players kill records in chat</t>
+13. |/topkills| - <t>Shows top kills UI with page selection</t>
+14. |/topkillschat (entity)| - <t>Shows top kills in chat</t>
+15. |/totalkills| - <t>Shows global kill count for all entities</t>
+16. |/totalkillschat| - <t>Shows global kill count in chat for all entities</t>
+17. |/killchat true/false| - <t>turns on/off kill messages in chat</t>
+18. |/totalkills| - <t>Shows global kill count for all entities</t>
+19. |/leadkills| - <t>Shows leaderboads UI</t>
+20. |/pstats| - <t>shows your harvest records UI</t>
+21. |/pstatschat| - <t>shows your harvest records in chat</t>
+22. |/pstats (playername)| - <t>Shows another players harvest record UI</t>
+23. |/pstatschat (playername)| - <t>Shows another players harvest record in chat</t>
+24. |/topstats| - <t>shows top player harvest stats UI</t>
+</comm>
