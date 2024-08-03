90740e3f16005126b8c2c0767f025cf5b0af417a
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-03 15:31:39 +0800

    Create TimeOfDay.cs

---
+++ TimeOfDay.cs
@@ -0,0 +1,13 @@
+<perm>
+1. |tod.nightlength| <t>Show/change the current setting for nightLength </t>
+2. |tod.daylength| <t>Show/change the current setting for dayLength </t>
+3. |tod.freezetime| <t>Toggle the progression of time </t>
+4. |tod.skipday| <t>Skip the current daytime </t>
+5. |tod.skipnight| <t>Skip the current nighttime </t>
+
+</perm>
+
+
+<comm>
+1. |/tod| <t> -- Show current time/day settings</t>
+</comm>
