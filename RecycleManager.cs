3c1830ca7c97c9c7ef366ae007ddfad8e274a36e
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-03 20:51:52 +0800

    Update RecycleManager.cs

--- RecycleManager.cs
+++ RecycleManager.cs
@@ -1,1 +1,8 @@
+<perm>
+1. |recyclemanager.admin| -- <t>Allows all commands, and allows using the administration panel while viewing a recycler.</t>
+</perm>
 
+<comm>
+1. |recyclemanager.add <item id or short name> | -- <t>Adds the specified item to the Override output section of the config.</t>
+2. |recyclemanager.reset <item id or short name> |-- <t>Adds or updates the specified item in the Override output section of the config.</t>
+</comm>
