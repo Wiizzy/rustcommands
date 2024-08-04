4e5d739c6dbaecbe456a6696f5dbcaad581fa038
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-04 11:44:01 +0800

    Create SortButton.cs

---
+++ SortButton.cs
@@ -0,0 +1,8 @@
+<perm>
+|sortbutton.use| -- <t>Allows players to see the sort button and use all features of the plugin.</t>
+</perm>
+
+<comm>
+|sortbutton| -- <t>Enable/Disable Sort Button.</t>
+|sortbutton <sort | type>| -- <t>change sort type.</t>
+</comm>
