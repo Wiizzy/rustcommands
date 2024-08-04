96acbd503089cc0b6545acc6834dffadb53fd0a6
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-04 11:50:41 +0800

    Update CupboardList.cs

--- CupboardList.cs
+++ CupboardList.cs
@@ -1,1 +1,8 @@
+<perm>
+|cupboardlist.able| <t>Required to use the command</t>
+</perm>
 
+<comm>
+|/cupauth| <t>Displays who is authorized on the tool cupboard you're looking at.</t>
+|/cupowner| <t>Displays who placed the tool cupboard you're looking at.</t>
+</comm>
