3ae94d91723fce73366e23a2774229e2a5d16c06
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-04 11:56:05 +0800

    Create BuildingInfo.cs

---
+++ BuildingInfo.cs
@@ -0,0 +1,12 @@
+<perm>
+|buildinginfo.scan| -- <t>Scan the building.</t>
+|buildinginfo.owner| -- <t>Get the owner of the building.</t>
+|buildinginfo.authed| -- <t>Get players authed in TC.</t>
+|buildinginfo.bypass| -- <t>Disallow scanning your building.</t>
+</perm>
+
+<comm>
+|/scan| -- <t>Scan the building.</t>
+|/owner| -- <t>Get the owner of the building.</t>
+|/authed| -- <t>Get players authed in TC.</t>
+</comm>
