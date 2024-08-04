d26d222201acb14b9a4e842ca5197a8772252195
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-04 11:58:12 +0800

    Create CraftingController.cs

---
+++ CraftingController.cs
@@ -0,0 +1,17 @@
+<perm>
+|craftingcontroller.instantbulkcraft| -- <t>give player permissions to instantly bulk craft all items</t>
+|craftingcontroller.blockitems| -- <t>allows use of the block and unblock craft-able items commands</t>
+|craftingcontroller.itemrate| -- <t>allows setting of individual item craft rates</t>
+|craftingcontroller.craftingrate| -- <t>allows craftrate command</t>
+|craftingcontroller.setbenchlvl| -- <t>allows use of benchlvl command</t>
+|craftingcontroller.setskins --allows setskin command|
+</perm>
+
+<comm>
+|craftrate| -- <t>shows current craft rate and can set new craft rate with integer.</t>
+|crafttime| -- <t>set individual craft time of items crafttime sleepingbag 5 or crafttime sleepingbag default.</t>
+|blockitem| -- <t>blocks an item from being craft-able block sleepingbag.</t>
+|unblockitem| -- <t>makes an item craft-able unblock sleepingbag.</t>
+|setcraftskin| -- <t>sets default craft skin if no other skin is selected setcraftskin sleepingbag workshopid.</t>
+|benchlvl| -- <t>sets the workbench level required to craft an item. Must be a integer between 0 (no-workbench) and 3 (tier 3 workbench) benchlvl sleepingbag 3</t>
+</comm>
