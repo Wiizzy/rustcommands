39804a663dcb9f295c16a35cf19d6d50e613f28b
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-04 11:54:18 +0800

    Create AmmoHUD.cs

---
+++ AmmoHUD.cs
@@ -0,0 +1,9 @@
+<perm>
+|ammohud.use| -- <t>Enables HUD for players</t>
+</perm>
+
+<comm>
+|/ammohud| -- <t>(Sends available commands and positions)</t>
+|/ammohud toggle| -- <t>(Toggles on/off HUD)</t>
+|/ammohud {position}| -- <t>(Allows player to reposition Ammo HUD on their screen)</t>
+</comm>
