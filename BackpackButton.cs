8361fb6c32aad3dc124f53b224297f4406f3c640
Wiizzy <34699043+Wiizzy@users.noreply.github.com>
2024-08-04 11:47:05 +0800

    Create BackpackButton.cs

---
+++ BackpackButton.cs
@@ -0,0 +1,8 @@
+<perm>
+|backpackbutton.use| -- <t>Allows players to see and toggle the backpack button.</t>
+</perm>
+
+<comm>
+|backpackui| -- <t>Displays help information about how to use the command, as well as the available button positions.</t>
+|backpackui <position> (Left | Right | Toggle | Off)| -- <t>Changes your preferred backpack button position. Use Toggle to show or hide the button without changing preferred position. Use Off to hide the backpack button.</t>
+</comm>
